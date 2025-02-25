dc175e6bfb472a68867b7f7e66d4163920bdfcac
Eldho <eldhobaby40@gmail.com>
2025-02-25 20:29:29 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# EtLab-Survey
+Complete pending surveys of etlab.
+Made for ICET-etlab, might work for others too.
+## Usage
+### Clone Repository
+```sh
+git clone https://github.com/e3ob/EtLab-Survey.git
+cd EtLab-Survey
+```
+### Fill envs
+```sh
+cp sample.env .env
+```
+and edit the .env file.
+**Quotes are are compulsory !!**
+###  Methods
+> More methods to be added !
+#### Using venv
+```sh
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,65 @@
+from requests import Session
+from bs4 import BeautifulSoup
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+COOKIEKEY = "ICETSESSIONID"
+etlabLink = "https://icet.etlab.in"
+
+session = Session()
+session.headers.update({"User-Agent": userAgent})
+
+session.post(
+    f"{etlabLink}/user/login",
+    data={
+        "LoginForm[username]": getenv("USERNAME"),
+        "LoginForm[password]": getenv("PASSWORD"),
+        "yt0": "",
+    },
+    headers={"Content-Type": "application/x-www-form-urlencoded"},
+)
+
+surveyData = BeautifulSoup(
+    session.get(f"{etlabLink}/survey/user/viewall").text, "html.parser"
+).find("table", class_="section table table-bordered")
+surveyList = []
+for tr in surveyData.find_all("tr"):
+    linkEle = tr.select("td:nth-child(7) > span")
+    if not linkEle:
+        continue
+    if "faculty" in tr.select("td:nth-child(3)")[0].text.lower():
+        # add them to the surveyYes list
+        pass
+    surveyList.append(
+        {
+            "link": etlabLink + linkEle[0].a["href"],
+            "name": tr.select("td:nth-child(2)")[0].text.strip(),
+        }
+    )
+print(surveyList)
+if not surveyList:
+    print("No surveys pending.")
+    exit()
+
+
+def surveySubmitter(SurveyLink: str):
+    soup = BeautifulSoup(session.get(SurveyLink).text, "html.parser")
+    surveySoup = soup.select("ul.survey")[0]
+    data = {"environment": "1", "yt0": ""}
+    for question in surveySoup.find_all("div", class_="answer"):
+        ans = question.find("input", {"type": "radio"})
+        data[ans.get("name")] = ans.get("value")
+        # print(ans.text, ans.attrs, ans.find_next_sibling().string.strip())
+    print(data)
+    print(session.post(SurveyLink, data=data).text)
+
+
+for survey in surveyList:
+    surveySubmitter(survey["link"])
+
+print(
+    f"Completed {len(surveyList)} survey(s).\n"
+    f"Surveys \n {'\n'.join([i['name'] for i in surveyList])}"
+)
